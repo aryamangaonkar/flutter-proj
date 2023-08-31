98b4af23af842a257511bdaf48fae91c6ddfa707
aryamangaonkar <143328929+aryamangaonkar@users.noreply.github.com>
2023-08-31 23:11:25 +0530

    Update project_1.dart

--- project_1.dart
+++ project_1.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-//import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_core/firebase_core.dart';
 //import 'package:flutter_application_1/login_page.dart';
 
 void main() {
